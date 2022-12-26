5c693b9a01c614d9cca017e8c7c28f5f6ca6a1bc
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-26 19:56:10 +0900

    asd

--- hyp_metric/hyptorch/nn.py
+++ hyp_metric/hyptorch/nn.py
@@ -68,9 +68,9 @@ class HypClassifer(nn.Module):
 
     def forward(self, x):
         # x = repeat(x, "b d -> b nc d", nc=self.num_classes)
-        x_norm = torch.norm(x, dim=-1, keepdim=True)
+        x_norm = torch.norm(x, dim=-1, keepdim=True).detach()
         x = x * x_norm
-        weight_norm = torch.norm(self.weight, dim=-1, keepdim=True)
+        weight_norm = torch.norm(self.weight, dim=-1, keepdim=True).detach()
         weight = self.weight * weight_norm
         # x_norm = torch.exp(x_norm * self.c)
         # weight_norm = torch.exp(weight_norm * self.c)
