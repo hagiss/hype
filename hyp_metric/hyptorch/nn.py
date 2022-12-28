4f8229e5943d1d52e4966eb70a34cd6521060c2a
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-28 16:00:47 +0900

    asd

--- hyp_metric/hyptorch/nn.py
+++ hyp_metric/hyptorch/nn.py
@@ -28,7 +28,7 @@ class HyperbolicMLR(nn.Module):
             c = torch.as_tensor(self.c).type_as(x)
         else:
             c = torch.as_tensor(c).type_as(x)
-        to_poincare = ToPoincare(c, clip_r=1).cuda()
+        to_poincare = ToPoincare(c, clip_r=1).type_as(x)
         x, _ = to_poincare(x)
         p_vals_poincare = pmath.expmap0(self.p_vals, c=c)
         # p_vals_poincare = self.p_vals
