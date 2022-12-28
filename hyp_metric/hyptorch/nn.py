cebadbe437db85e592a8ffaf8d86f9aa82ef84b5
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-28 17:09:27 +0900

    asd

--- hyp_metric/hyptorch/nn.py
+++ hyp_metric/hyptorch/nn.py
@@ -93,7 +93,7 @@ class HypClassifer(nn.Module):
         # logits = logits * x_norm
         # logits = x + self.bias
         # logits = -x * norm + self.bias
-        return logits * x_norm.pow(2) * torch.norm(self.weight, dim=-1, keepdim=True)
+        return logits * x_norm.pow(2) * torch.norm(self.weight, dim=-1, keepdim=True).T.squeeze()
 
 class HypLinear(nn.Module):
     def __init__(self, in_features, out_features, c, bias=True):
