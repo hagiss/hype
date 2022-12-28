c57ea6837ec0626356446b8957521c29b8e57cdb
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-28 16:07:03 +0900

    ASD

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ def train(epoch):
         sys.stdout.write('\r')
         sys.stdout.write('| Epoch [%3d/%3d] Iter[%3d/%3d]\t\tLoss: %.4f Acc@1: %.3f%% Norm: %.3f'
                          % (epoch, num_epochs, batch_idx + 1,
-                            (len(trainset) // batch_size) + 1, loss.item(), 100. * correct / total, norm.mean().item()))
+                            (len(trainset) // batch_size) + 1, loss.item(), 100. * correct / total, norm.var().item()))
         sys.stdout.flush()
 
 
@@ -262,7 +262,7 @@ def test(epoch):
         # Save checkpoint when best model
         acc = 100. * correct / total
         print("\n| Validation Epoch #%d\t\t\tLoss: %.4f Acc@1: %.2f%% Norm: %.3f" % (
-        epoch, loss.item(), acc, norm.mean().item()))
+        epoch, loss.item(), acc, norm.var().item()))
 
         if acc > best_acc:
             print('| Saving Best model...\t\t\tTop1 = %.2f%%' % (acc))
