9ffeb5d47d47b419735c495a99b29f19959b5a7b
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-29 14:31:01 +0900

    asd

--- hyp_metric/hyptorch/nn.py
+++ hyp_metric/hyptorch/nn.py
@@ -91,14 +91,14 @@ class HypClassifer(nn.Module):
         # weight = pmath.expmap0(self.weight, c=self.c)
 
         logits = pmath.mobius_matvec(x, self.weight, c=self.c) # + bias
-        print(logits.shape)
+        # print(logits.shape)
         # logits = logits * x_norm
         # logits = x + self.bias
         # logits = -x * norm + self.bias
         x_norm = torch.norm(x, dim=-1)
         weight_norm = torch.norm(self.weight, dim=-1, keepdim=True).T.squeeze()
-        print(weight_norm.shape)
-        print(x_norm.shape)
+        # print(weight_norm.shape)
+        # print(x_norm.shape)
         c=0.1
         return logits * (weight_norm * x_norm)
 
