2e6fad3ead9c9acdac99d52c0a204927f96faf4f
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-29 17:17:09 +0900

    asd

--- hyp_metric/hyptorch/nn.py
+++ hyp_metric/hyptorch/nn.py
@@ -87,14 +87,14 @@ class HypClassifer(nn.Module):
         # dist =
         # out = torch.sign(eout) * weight_norm * dist
 
-        x = pmath.expmap0(x, c=self.c)
-        bias = pmath.expmap0(self.bias, c=self.c)
-        weight = pmath.expmap0(self.weight, c=self.c)
+        # x = pmath.expmap0(x, c=self.c)
+        # bias = pmath.expmap0(self.bias, c=self.c)
+        # weight = pmath.expmap0(self.weight, c=self.c)
 
         x_norm = torch.norm(x, dim=-1, keepdim=True)
-        weight_norm = torch.norm(weight, dim=-1, keepdim=True).T.squeeze()
+        weight_norm = torch.norm(self.weight, dim=-1, keepdim=True).T.squeeze()
 
-        logits = x @ weight.T + bias
+        logits = x @ self.weight.T + self.bias
         c=0.1
         return (logits.detach() * (x_norm * weight_norm) + logits * (x_norm * weight_norm).detach()) / 2
 
