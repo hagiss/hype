e35bcb66c5b0329a83e1c4d1a6dbab207dcadf3e
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-29 14:08:03 +0900

    asd

--- main.py
+++ main.py
@@ -99,6 +99,7 @@ transform_train = transforms.Compose([
 ]) # meanstd transformation
 
 transform_test = transforms.Compose([
+    transforms.RandomCrop(32),
     transforms.ToTensor(),
     transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
 ])
