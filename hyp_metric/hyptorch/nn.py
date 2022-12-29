554c27f6d37f2ffef8f249db49931b0def42a6e9
hagiss <66506438+hagiss@users.noreply.github.com>
2022-12-29 17:45:34 +0900

    asd

--- hyp_metric/hyptorch/nn.py
+++ hyp_metric/hyptorch/nn.py
@@ -97,8 +97,8 @@ class HypClassifer(nn.Module):
         logits = x @ (self.weight.T / weight_norm) + self.bias
         # logits2 = x.detach() @ self.weight.T + self.bias
         c=0.1
-        # return (logits.detach() * (x_norm) + logits * (x_norm)) / 2
-        return logits * x_norm
+        return (logits.detach() * (x_norm) + logits * (x_norm)) / 2
+        # return logits * x_norm
 
 class HypLinear(nn.Module):
     def __init__(self, in_features, out_features, c, bias=True):
