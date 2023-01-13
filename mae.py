a85a54c0816e77bc6f28c56c959139e8ef072a51
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-13 14:02:13 +0900

    mae

--- mae.py
+++ mae.py
@@ -15,7 +15,7 @@ outputs = model(**inputs)
 last_hidden_states = outputs.last_hidden_state
 
 # print(last_hidden_states.shape)
-last_hidden_states.squeeze()
+last_hidden_states.squeeze().detach().numpy()
 
 dis_mat = distance_matrix(last_hidden_states, last_hidden_states)
 
