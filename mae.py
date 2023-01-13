0f75ce5db253c25f4be17c9de341a13a02046b5e
hagiss <66506438+hagiss@users.noreply.github.com>
2023-01-13 14:01:27 +0900

    mae

--- mae.py
+++ mae.py
@@ -1,6 +1,8 @@
 from transformers import AutoFeatureExtractor, ViTMAEModel
 from PIL import Image
 import requests
+from scipy.spatial import distance_matrix
+import numpy as np
 
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
@@ -12,4 +14,22 @@ inputs = feature_extractor(images=image, return_tensors="pt")
 outputs = model(**inputs)
 last_hidden_states = outputs.last_hidden_state
 
-print(last_hidden_states.shape)
+# print(last_hidden_states.shape)
+last_hidden_states.squeeze()
+
+dis_mat = distance_matrix(last_hidden_states, last_hidden_states)
+
+def delta_hyp(dismat):
+    """
+    computes delta hyperbolicity value from distance matrix
+    """
+
+    p = 0
+    row = dismat[p, :][np.newaxis, :]
+    col = dismat[:, p][:, np.newaxis]
+    XY_p = 0.5 * (row + col - dismat)
+
+    maxmin = np.max(np.minimum(XY_p[:, :, None], XY_p[None, :, :]), axis=1)
+    return np.max(maxmin - XY_p)
+
+print(delta_hyp(dis_mat))
